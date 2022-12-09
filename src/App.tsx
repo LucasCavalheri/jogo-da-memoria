df173da549a76b35c773613d34b9afb4106f587a
LucasCavalheri <cav.catecumenal@gmail.com>
2022-12-09 16:31:15 -0300

    Feat: Instala o Styled Components e já prepara os Componentes estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href=''>
+          <img src={logoImage} width='200' />
+        </C.LogoLink>
 
-export default App
+        <C.InfoArea>...</C.InfoArea>
+
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>...</C.GridArea>
+    </C.Container>
+  );
+};
+
+export default App;
